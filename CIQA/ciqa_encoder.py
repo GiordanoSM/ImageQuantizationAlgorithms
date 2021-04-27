40ef8ce0dc1492306de5c46d1479b20adf76a1a0
GiordanoSM <giordano@suffert.com>
2021-04-27 11:38:55 -0300

    Blocos separados e informação lateral calculada

--- CIQA/ciqa_encoder.py
+++ CIQA/ciqa_encoder.py
@@ -1,6 +1,8 @@
 import filename_handler as fh
 import bitstring as bs
 import sys
+from PIL import Image, UnidentifiedImageError
+import numpy as np
 
 #PIL, numpy, bitstring
 def encoder (N, M, filename, directory):
@@ -10,14 +12,84 @@ def encoder (N, M, filename, directory):
   filename_result = fh.add_path_cqa(directory, no_path_name) #Cria o nome do arquivo comprimido junto com o diretório alvo
 
   try:
-    with open(filename_result, 'wb') as f_write:
-      pass
-    pass
+    with Image.open(filename) as im:
+      with open(filename_result, 'wb') as f_write:
+
+        if im.mode != 'L': #Verifica se a imagem está em escala de cinza
+          raise WrongFormat()
+
+        data = np.asarray(im)
+
+        data_out, paddings = adaptiveQuantizer(N, M, data)
+
+        header.tofile(f_write)
+
+        bs.Bits(uint=N, length=8).tofile(f_write)
+        bs.Bits(uint=M, length=8).tofile(f_write)
+
+        bs.Bits(uint=paddings[0], length=8).tofile(f_write)
+        bs.Bits(uint=paddings[1], length=8).tofile(f_write)
+
+        data_out.tofile(f_write)
+
+        f_write.seek(0)
+        f_write.write((header[:4] + bs.Bits(uint= paddings[2], length= 4)).tobytes()) #Informa a quantidade final de padding no cabeçalho
 
   except IOError as ioe:
-    sys.exit('Arquivo ou diretório "{}" não existente.'.format(ioe.filename))
+    sys.exit('ERRO: Arquivo ou diretório "{}" não existente.'.format(ioe.filename))
+
+  except WrongFormat:
+    sys.exit('ERRO: O programa aceita somente imagem em escala de cinza.')
 
   print("Terminado! Criado arquivo '{}'.\n".format(filename_result))
+
+#-------------------------------------
+
+class WrongHeader(Exception):
+  pass
+
+class WrongFormat(Exception):
+  pass
+
+#-------------------------------------
+
+def adaptiveQuantizer(N, M, data):
+
+  padded_data, paddings = pad(N, data)
+
+  blocks = getBlocks(N, padded_data)
+
+  info = []
+  for b in blocks:
+    info.append((np.min(b),np.max(b)))
+
+  bpp = np.ceil(np.log2(M))
+
+  paddings.append(0)
+
+  return bs.Bits(bin='0b'), paddings
+
+#-------------------------------------
+#Bem comportado até padding maximo de 255
+def pad (N, data):
+  missing = lambda x: (N - (x % N))%N
+  paddings = [np.uint8(missing(data.shape[0])), np.uint8(missing(data.shape[1]%N))]
+  
+  return np.pad(data, ((0,missing(data.shape[0])), (0,missing(data.shape[1])))), paddings
+
+#-------------------------------------
+
+def getBlocks(N, data):
+  n_blocks_h = data.shape[0]/N
+  n_blocks_v = data.shape[1]/N
+
+  blocks = []
+
+  for l in range(0, data.shape[0], N):
+    for c in range(0, data.shape[1], N):
+      blocks.append(data[l:l+N,range(c, c+N)])
+
+  return blocks
 
 #-------------------------------------
 
@@ -27,13 +99,16 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
       M = int(sys.argv[2])
     else:
-      M = input("Informe o número de níveis (M) do quantizador: ")
+      M = int(input("Informe o número de níveis (M) do quantizador: "))
 
   else:
-    N = input("Informe o tamanho do bloco (N) a ser utilizado: ")
-    M = input("Informe o número de níveis (M) do quantizador: ")
+    N = int(input("Informe o tamanho do bloco (N) a ser utilizado: "))
+    M = int(input("Informe o número de níveis (M) do quantizador: "))
+
+  if N > 255 or N < 0 or M > 255 or M < 0:
+    sys.exit("ERRO: Informe valores de N e M entre 0 e 255.")
 
   filename = input("Informe o nome (caminho) do arquivo a ser codificado: ")
   directory = input("Informe o nome do diretório do resultado (será o atual caso não informado): ")
-  
+
   encoder(N, M, filename, directory)
