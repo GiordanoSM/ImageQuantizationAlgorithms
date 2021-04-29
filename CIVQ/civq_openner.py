394b8943a8850dd30d51e2682f45b35c2e1893f5
GiordanoSM <giordano@suffert.com>
2021-04-29 01:41:13 -0300

    Extraídos os blocos

--- CIVQ/civq_openner.py
+++ CIVQ/civq_openner.py
@@ -11,22 +11,54 @@ def openner (filename, show_image=True):
     sys.exit('Erro: Esperado arquivo de extensão: ".cvq".')
 
   try:
+    print("Começando a decodificação... (Pode demorar um pouco)")
+    start = time.time()
+
     header = bs.Bits(filename= filename, length=8)#Header sendo F + numero de bits de padding no final do arquivo
   
     padding_end = checkHeader(header)#Extrai o tamanho do padding do final do arquivo do header e verifica-o
 
     M = bs.Bits(filename= filename, length=8, offset=8).uint + 1#Numero de elementos no codebook
     L = bs.Bits(filename= filename, length=8, offset=16).uint#Tamanho de cada bloco
-    
-    codebook, offset = codebookParser(filename, M, L, 24)
 
+    codebook, offset = codebookParser(filename, M, L, 24)
     #print(codebook, offset)
+
+    n_blocos_h = bs.Bits(filename= filename, length=8, offset=offset).uint#informa a quantidade de blocos na horizontal
+
+    padding_lin = bs.Bits(filename= filename, length=8, offset=offset+8).uint#número de linhas colocadas como padding
+    padding_col = bs.Bits(filename= filename, length=8, offset=offset+16).uint#número de colunas colocadas como padding
+
+    blocks = decoder(codebook, filename, M, L, padding_end, offset+24)#Extrai os blocos individualmente dos dados
+
+    end = time.time()
+    print('Demorou: {} segundos'.format(end - start))
 
   except IOError as ioe:
     sys.exit('ERRO: Arquivo ou diretório "{}" não existente.'.format(ioe.filename))
 
   except WrongHeader:
     sys.exit('Erro: Header do arquivo não condiz com o esperado.')
+
+#-------------------------------------------
+#Tem a função de extrair todos os blocos existentes mantendo seus formatos originais
+def decoder(codebook, filename, M, L, padding_end, offset):
+  bpb = int(np.ceil(np.log2(M)))
+  N = int(np.sqrt(L))
+
+  data = bs.Bits(filename=filename, offset= offset)
+
+  if padding_end != 0:
+    data = data[:-padding_end]
+
+  blocks = []
+
+  while data != bs.Bits(bin= "0b"):
+    idx = data[:bpb].uint
+    blocks.append(codebook[idx].reshape((N,N)))
+    data = data[bpb:]
+
+  return blocks
 
 #------------------------------------------
 def codebookParser (filename, M, L, offset):
