997379552349ea4462ee58d740c502ed717107f5
GiordanoSM <giordano@suffert.com>
2021-05-01 00:14:29 -0300

    Parte4 completa - faltando somente o relatório

--- Dithering/teste.py
+++ Dithering/teste.py
@@ -30,16 +30,13 @@ sizes_colour=[]
 for b in [False, True]:
   for m in M_l:
     ce.encoder(filename_enc, m, directory_enc, b)
-    results.append(co.openner(filename_dec, show_image=True))
+    results.append(co.openner(filename_dec, show_image=False))
     bpp.append(os.stat(filename_dec).st_size*8/total_pixels)
     sizes.append(os.stat(filename_dec).st_size)
     ce.encoder(filename_enc_colour, m, directory_enc, b)
-    results_colour.append(co.openner(filename_dec_colour, show_image=True))
+    results_colour.append(co.openner(filename_dec_colour, show_image=False))
     bpp_colour.append(os.stat(filename_dec_colour).st_size*8/total_pixels)
     sizes_colour.append(os.stat(filename_dec_colour).st_size)
-
-  print("Cinza", sizes)
-  print("Color", sizes_colour)
 
 with Image.open(filename_enc) as im:
   data = np.asarray(im).astype(np.int16)
@@ -58,8 +55,8 @@ colors2 = ['red', 'orange']
 
 fig, ax = plt.subplots()
 for i in range(2):
-  ax.scatter(bpp[i*2:i*2+len(M_l)], mse[i*2:i*2+len(M_l)],c=colors1[i] ,label= "GrayScale {} Dithering".format('with' if i else 'without'))
-  ax.scatter(bpp_colour[i*2:i*2+len(M_l)], mse_colour[i*2:i*2+len(M_l)],c=colors2[i] ,label= "Colour {} Dithering".format('with' if i else 'without'))
+  ax.scatter(bpp[i*len(M_l):i*len(M_l)+len(M_l)], mse[i*len(M_l):i*len(M_l)+len(M_l)],c=colors1[i] ,label= "GrayScale {} Dithering".format('with' if i else 'without'))
+  ax.scatter(bpp_colour[i*len(M_l):i*len(M_l)+len(M_l)], mse_colour[i*len(M_l):i*len(M_l)+len(M_l)],c=colors2[i] ,label= "Colour {} Dithering".format('with' if i else 'without'))
 ax.set_xlabel('bpp')
 ax.set_ylabel('MSE')
 ax.set_title('MSE x BPP')
@@ -69,7 +66,7 @@ ax.legend()
 fig, ax = plt.subplots()
 ax.plot(bpp[:len(M_l)], psnr[:len(M_l)],label="GrayScale without Dithering")
 ax.plot(bpp[len(M_l):], psnr[len(M_l):],label="GrayScale with Dithering")
-ax.plot(bpp_colour[:v], psnr_colour[:len(M_l)],label="Colour with Dithering")
+ax.plot(bpp_colour[:len(M_l)], psnr_colour[:len(M_l)],label="Colour with Dithering")
 ax.plot(bpp_colour[len(M_l):], psnr_colour[len(M_l):],label="Colour without Dithering")
 ax.set_xlabel('bpp')
 ax.set_ylabel('PSNR')
@@ -87,4 +84,4 @@ for i in range(len(results)//2):
   im = Image.fromarray(results[i+len(M_l)])
   im.save("../Plots/Parte4/lena_{}_dit.png".format(M_l[i]))
   im = Image.fromarray(results_colour[i+len(M_l)])
-  im.save("../Plots/Parte4/lena_colour_{}.png".format(M_l[i]))
+  im.save("../Plots/Parte4/lena_colour_{}_dit.png".format(M_l[i]))
